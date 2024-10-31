f4a148016808269e5e8cc269e822a84288575335
RACHURI HARISH <hr2946352@gmail.com>
2024-10-31 16:31:23 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,6 +2,7 @@ create database taskmanager;
 use taskmanager;
 
 create table Accounts(
+    Username varchar(50) not null,
     Email varchar(80) primary key,
     passwords varchar(80) not null
 );
